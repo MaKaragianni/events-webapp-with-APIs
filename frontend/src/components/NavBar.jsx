c80dba61c7a8980a05e9d279dadd1dc9709b12df
Zein Rafie <zeinrafie1@gmail.com>
2026-06-26 15:42:34 +0100

    navbar working with conditional rendering

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,17 +1,24 @@
 import LogoutButton from "./LogoutButton";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import { authClient } from "../services/authentication";
 
 
 // need to make logout button dynamic (login / logout) checking session exists
 // don't show profile to logged out
 
 const NavBar = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { pathname } = location
+
+    const { data: session, isPending } = authClient.useSession();
+    if (isPending) return null;
+
     return (
         <nav style={{borderBottom : "1px solid black"}}>
-            <button onClick={() => navigate("/feed")}>Feed</button>
-            <button onClick={() => navigate("/profile")}>Profile</button>
-            <LogoutButton/>
+            {pathname !== "/feed" && <button onClick={() => navigate("/feed")}>Feed</button>}
+            {session && (pathname !== "/profile") && <button onClick={() => navigate("/profile")}>Profile</button>}
+            {session ? <LogoutButton/> : <button onClick={() => navigate("/login")}>Login</button>}
         </nav>
         
     )
